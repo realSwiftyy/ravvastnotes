2dfabf50671683cb0d7c58b7ab170830b83f3350
realSwiftyy <ydenekjan@gmail.com>
2023-12-19 22:30:28 +0100

    create and remove fixed

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 const icons = document.querySelectorAll('.ico')
 const notesContainer = document.querySelector('.notes-container')
 const createBtn = document.querySelector('.btn-create')
-const outputWrapper = document.querySelector('.output-container')
-let notesArr = []
+const outputContainer = document.querySelector('.output-container')
 
-function updateUI (color, ghost)  {
-    notesContainer.insertAdjacentHTML('beforeend', `<div class="ico ${color}">${notesArr[notesArr.length - 1]}</div>`)
-    if (ghost) {
-        notesContainer.lastElementChild.classList.add('ghost')
-    }
-}
+
 
 icons.forEach(icon => {
-    icon.addEventListener('mousedown', () => {
-        notesArr.push(icon.innerHTML)
-        let ghost = icon.classList.contains('ghost')
+    icon.addEventListener('click', () => {
         let color = icon.classList.contains('blue') ? 'blue' : 'red'
-        updateUI(color, ghost)
+        notesContainer.insertAdjacentHTML('beforeend', `<div class="ico ${color} note">${icon.innerHTML}</div>`)
+        if (icon.classList.contains('ghost')) notesContainer.lastElementChild.classList.add('ghost')
+
+
     })
+})
+
+notesContainer.addEventListener('click', event => {
+    if (event.target.classList.contains('note'))
+        notesContainer.removeChild(event.target)
+
 })
 
 createBtn.addEventListener('mousedown', () => {
     let originalContent = document.body.innerHTML
 
-    document.body.innerHTML = document.querySelector('.output-container')
+    document.body.innerHTML = outputContainer.innerHTML
+
     window.print()
     document.body.innerHTML = originalContent
+
 
     alert('Please refresh the site!')
 })
