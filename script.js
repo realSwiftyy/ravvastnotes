61fb46c9ac125530ed8a35528fe2cb02d62e9e9c
realSwiftyy <ydenekjan@gmail.com>
2023-12-20 14:37:27 +0100

    desktop working

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-const icons = document.querySelectorAll('.ico')
-const notesContainer = document.querySelector('.notes-container')
-const createBtn = document.querySelector('.btn-create')
-const outputContainer = document.querySelector('.output-container')
+function main() {
+    const icons = document.querySelectorAll('.ico')
+    const notesContainer = document.querySelector('.notes-container')
+    const createBtn = document.querySelector('.btn-create')
 
 
+    icons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            let color = icon.classList.contains('blue') ? 'blue' : 'red'
+            notesContainer.insertAdjacentHTML('beforeend', `<div class="ico ${color} note">${icon.innerHTML}</div>`)
+            if (icon.classList.contains('ghost')) notesContainer.lastElementChild.classList.add('ghost')
 
-icons.forEach(icon => {
-    icon.addEventListener('click', () => {
-        let color = icon.classList.contains('blue') ? 'blue' : 'red'
-        notesContainer.insertAdjacentHTML('beforeend', `<div class="ico ${color} note">${icon.innerHTML}</div>`)
-        if (icon.classList.contains('ghost')) notesContainer.lastElementChild.classList.add('ghost')
 
+        })
+    })
+
+    notesContainer.addEventListener('click', event => {
+        if (event.target.classList.contains('note'))
+            notesContainer.removeChild(event.target)
 
     })
-})
 
-notesContainer.addEventListener('click', event => {
-    if (event.target.classList.contains('note'))
-        notesContainer.removeChild(event.target)
+    createBtn.addEventListener('mousedown', () => {
+        const outputContainer = document.querySelector('.output-container')
 
-})
+        let originalContent = document.body.innerHTML
+        let sheetName = document.querySelector('#input').value
 
-createBtn.addEventListener('mousedown', () => {
-    let originalContent = document.body.innerHTML
+        document.body.innerHTML = outputContainer.innerHTML
+        document.body.insertAdjacentHTML('afterbegin', `<h1 class="sheet-name">${sheetName}</h1>`)
 
-    document.body.innerHTML = outputContainer.innerHTML
+        window.print()
 
-    window.print()
-    document.body.innerHTML = originalContent
+        document.body.innerHTML = originalContent
 
+        main()
+    })
 
-    alert('Please refresh the site!')
-})
+}
+
+main()
+
